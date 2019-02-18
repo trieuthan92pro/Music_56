311494ad32a3a60cde8e1a4e9b90828a6d8643b7
trieuthan92pro <trieu.van.than@framgia.com>
2019-02-18 12:28:46 +1030

    Fix comment

--- app/src/main/java/com/example/soundcloud/discover/DiscoverFragment.java
+++ app/src/main/java/com/example/soundcloud/discover/DiscoverFragment.java
@@ -1,21 +1,14 @@
 package com.example.soundcloud.discover;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.example.soundcloud.R;
-import com.example.soundcloud.data.source.SongRepository;
-import com.example.soundcloud.data.source.remote.SongRemoteDataSource;
 
 public class DiscoverFragment extends Fragment {
-    private DiscoverPresenter mPresenter;
 
     public DiscoverFragment() {
     }
@@ -24,11 +17,6 @@ public class DiscoverFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_discover, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
     }
 
 }
--- app/src/main/java/com/example/soundcloud/discover/DiscoverPresenter.java
+++ app/src/main/java/com/example/soundcloud/discover/DiscoverPresenter.java
@@ -1,17 +1,12 @@
 package com.example.soundcloud.discover;
 
-import com.example.soundcloud.BasePresenter;
 import com.example.soundcloud.data.source.SongRepository;
 
-public class DiscoverPresenter implements BasePresenter {
+public class DiscoverPresenter {
     private SongRepository mRepository;
+
     public DiscoverPresenter(SongRepository repository) {
         mRepository = repository;
-    }
-
-    @Override
-    public void start() {
-
     }
 
 }
