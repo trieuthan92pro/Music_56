8b9872816a88ec9c37097fc6a83af42e492b24ed
trieuthan92pro <trieu.van.than@framgia.com>
2019-02-20 17:51:51 +1030

    Create search screen UI

--- app/src/main/java/com/example/soundcloud/search/SearchActivity.java
+++ app/src/main/java/com/example/soundcloud/search/SearchActivity.java
@@ -3,11 +3,19 @@ package com.example.soundcloud.search;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.Group;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import com.example.soundcloud.R;
 
 public class SearchActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerSearchResult;
+    private RecyclerView mRecyclerSearchHistory;
+    private TextView mTextNumberSearchResult;
+    private Group mGroupHistorySearch;
+    private Group mGroupSearchResult;
 
     public static Intent getIntent(Context context) {
         return new Intent(context, SearchActivity.class);
@@ -17,5 +25,12 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        initView();
+    }
+
+    private void initView() {
+        mRecyclerSearchResult = findViewById(R.id.recycler_search_result);
+        mRecyclerSearchHistory = findViewById(R.id.recycler_view_search_history);
+        mTextNumberSearchResult = findViewById(R.id.text_number_search_result);
     }
 }
---
+++ app/src/main/java/com/example/soundcloud/search/SearchResultAdapter.java
@@ -0,0 +1,5 @@
+package com.example.soundcloud.search;
+
+public class SearchResultAdapter {
+
+}
