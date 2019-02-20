641f8e8cba230b5ac4d30d42fa8f60a76d86a2bf
trieuthan92pro <trieu.van.than@framgia.com>
2019-02-20 17:01:05 +1030

    Handle event

--- app/src/main/java/com/example/soundcloud/selected_genre_detail/SelectedGenreAdapter.java
+++ app/src/main/java/com/example/soundcloud/selected_genre_detail/SelectedGenreAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.soundcloud.R;
@@ -17,9 +18,11 @@ import com.example.soundcloud.data.model.Song;
 public class SelectedGenreAdapter extends RecyclerView.Adapter<SelectedGenreAdapter.ViewHolder> {
     private Genre mGenre;
     private LayoutInflater mLayoutInflater;
+    private OnItemClickListener mOnItemClickListener;
 
-    public SelectedGenreAdapter(Context context) {
+    public SelectedGenreAdapter(Context context, OnItemClickListener listener) {
         mLayoutInflater = LayoutInflater.from(context);
+        mOnItemClickListener = listener;
     }
 
     @NonNull
@@ -27,6 +30,7 @@ public class SelectedGenreAdapter extends RecyclerView.Adapter<SelectedGenreAdap
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = mLayoutInflater.inflate(R.layout.item_song, viewGroup, false);
         ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder.setListener(mOnItemClickListener);
         return viewHolder;
     }
 
@@ -49,11 +53,12 @@ public class SelectedGenreAdapter extends RecyclerView.Adapter<SelectedGenreAdap
         void onItemClick(int position);
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView mTextSongTitle;
         private TextView mTextSongArtist;
         private ImageView mImageArtwork;
         private Context mContext;
+        private OnItemClickListener mListener;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -61,6 +66,16 @@ public class SelectedGenreAdapter extends RecyclerView.Adapter<SelectedGenreAdap
             mTextSongArtist = itemView.findViewById(R.id.text_song_item_artist);
             mTextSongTitle = itemView.findViewById(R.id.text_song_item_title);
             mImageArtwork = itemView.findViewById(R.id.image_song_item_artwork);
+
+            mTextSongArtist.setOnClickListener(this);
+            mTextSongTitle.setOnClickListener(this);
+            mImageArtwork.setOnClickListener(this);
+            itemView.findViewById(R.id.image_item_song_download).setOnClickListener(this);
+            itemView.findViewById(R.id.image_item_song_option).setOnClickListener(this);
+        }
+
+        public void setListener(OnItemClickListener listener) {
+            mListener = listener;
         }
 
         public void bindData(Song song) {
@@ -70,6 +85,28 @@ public class SelectedGenreAdapter extends RecyclerView.Adapter<SelectedGenreAdap
             Glide.with(mContext).load(song.getArtworkUrl())
                     .error(R.drawable.ic_artwork_item_default)
                     .into(mImageArtwork);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.text_song_item_artist:
+                case R.id.text_song_item_title:
+                case R.id.image_song_item_artwork:
+                    mListener.onItemClick(getAdapterPosition());
+                    break;
+
+                case R.id.image_item_song_download:
+                    //TODO(1): handle download feature latter
+                    break;
+
+                case R.id.image_item_song_option:
+                    //TODO(2): handle menu option feature latter
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
